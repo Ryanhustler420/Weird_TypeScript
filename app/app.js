3d2c73592ded2fb727dbbedcf8ea2867a5d332b7
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-09 15:07:03 +0530

    Types

--- app/app.js
+++ app/app.js
@@ -1,2 +1,36 @@
 "use strict";
-console.log('hello');
+// string
+var myName = 'Gaurav';
+// myName = 28; // throw error
+// number
+var myAge = 27.5;
+// myAge = "Gaurav"; // throw error
+// boolean
+var hasHobbies = false;
+// hasHobbies = 1; // throw error
+// assign types : any
+var myRealAge;
+myRealAge = 27;
+// myRealAge = '27';
+// array
+var hobbies = ["Cooking", "Sports"];
+hobbies = ['A String'];
+// tuples : like has table
+var address = ["Superstreet", 99];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Grey"] = 0] = "Grey";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue";
+    Color[Color["Yellow"] = 3] = "Yellow";
+    Color[Color["purple"] = 4] = "purple";
+    Color[Color["pink"] = 5] = "pink"; // 5
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
+// any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 'M4' };
+console.log(car);
--- app/app.ts
+++ app/app.ts
@@ -1,1 +1,42 @@
-console.log('hello');
+// string
+let myName: string = 'Gaurav';
+// myName = 28; // throw error
+
+// number
+let myAge = 27.5;
+// myAge = "Gaurav"; // throw error
+
+// boolean
+let hasHobbies = false;
+// hasHobbies = 1; // throw error
+
+// assign types : any
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = '27';
+
+// array
+let hobbies: string[] = ["Cooking", "Sports"];
+hobbies = ['A String'];
+
+// tuples : like has table
+let address: [string, number] = ["Superstreet", 99];
+
+// enum
+enum Color {
+    Grey, // 0
+    Green = 100, // 100
+    Blue, // 101
+    Yellow = 3, // 3
+    purple, // 4
+    pink // 5
+}
+
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 'M4' }
+console.log(car);
