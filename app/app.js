4c2824d9b9335310a0faf49f4eb6656c3651aa14
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-11 17:09:42 +0530

    Namespaces
    help us to prevent code clutter or mix code

--- app/app.js
+++ app/app.js
@@ -1,129 +1,18 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Person = /** @class */ (function () {
-    // public username: string [is another way to create and assigning value at the time of initalazation ]
-    // constructor(name: string, type: string, public username: string) {
-    function Person(name, type) {
-        this.name = name;
-        this.type = type;
-        this.healt = 200;
+// Namespaces
+var MyMath;
+(function (MyMath) {
+    var PI = 3.14;
+    function calculateCircumference(diameter) {
+        return diameter * PI;
     }
-    Person.prototype.printAge = function () {
-        console.log(this.healt);
-        this.setType("CEO");
-    };
-    Person.prototype.setType = function (type) {
-        this.type = type;
-        console.log(this.type);
-    };
-    return Person;
-}());
-var person = new Person('Gaurav', 'Owner');
-person.printAge();
-// Inheritance
-var ThisUser = /** @class */ (function (_super) {
-    __extends(ThisUser, _super);
-    function ThisUser() {
-        var _this = _super.call(this, name, 'Employee') || this;
-        _this.name = "Gaurav";
-        _this.healt = 50;
-        return _this;
-        // console.log(this.type) can't access!
+    MyMath.calculateCircumference = calculateCircumference;
+    function calculateRectangle(width, length) {
+        return width * length;
     }
-    return ThisUser;
-}(Person));
-var gaurav = new ThisUser();
-console.log(gaurav);
-// Getters & Setters
-var Plant = /** @class */ (function () {
-    function Plant() {
-        this._species = "Default";
-    }
-    Object.defineProperty(Plant.prototype, "species", {
-        get: function () {
-            return this._species.toUpperCase();
-        },
-        set: function (value) {
-            value.length > 3 ? this._species = value : this._species = "Default";
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return Plant;
-}());
-var plant = new Plant();
-console.log(plant.species);
-plant.species = "AB";
-console.log(plant.species);
-plant.species = "Green Plant";
-console.log(plant.species);
-// Static Properties & Methods
-var Helper = /** @class */ (function () {
-    function Helper() {
-    }
-    Helper.calcCircumference = function (diameter) {
-        return this.PI * diameter;
-    };
-    Helper.PI = 3.14; // by default value
-    return Helper;
-}());
-console.log(Helper.PI * 2);
-console.log(Helper.calcCircumference(8));
-// Abstract Classes
-// it may be helpfull for basic setup for any classes not this class won't instanciat 
-// you can bundle some generall functionallty which should provide basci blue print to child calss
-// but remember it will never instanciat. so, Keep that in mind.
-var Project = /** @class */ (function () {
-    function Project() {
-        this.projectName = "Default";
-        this.budget = 0;
-    }
-    Project.prototype.clacBudget = function () {
-        return this.budget * 2;
-    };
-    return Project;
-}());
-var ITProject = /** @class */ (function (_super) {
-    __extends(ITProject, _super);
-    function ITProject() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    ITProject.prototype.changeName = function (name) {
-        // set the login the way you want to set
-        this.projectName = name;
-    };
-    return ITProject;
-}(Project));
-var newProject = new ITProject();
-console.log(newProject);
-newProject.changeName('Super IT Project');
-console.log(newProject);
-// private constructors
-var OnlyOne = /** @class */ (function () {
-    function OnlyOne(name) {
-        this.name = name;
-    }
-    OnlyOne.getInstance = function () {
-        if (!OnlyOne.instance) {
-            OnlyOne.instance = new OnlyOne('The Only One');
-        }
-        return OnlyOne.instance;
-    };
-    return OnlyOne;
-}());
-// Singleton Class which gives only one instance at runtime.
-// let wrong = new OnlyOne('The Only One'); // wrong way
-var right = OnlyOne.getInstance(); // right way
-console.log(right.name);
+    MyMath.calculateRectangle = calculateRectangle;
+})(MyMath || (MyMath = {}));
+var PI = 2.99;
+console.log(MyMath.calculateRectangle(10, 20));
+console.log(MyMath.calculateCircumference(20));
+console.log(PI);
--- app/app.ts
+++ app/app.ts
@@ -1,131 +1,17 @@
-class Person {
-    name: string;
-    private type: string;
-    protected healt: number;
-    // public username: string [is another way to create and assigning value at the time of initalazation ]
-    // constructor(name: string, type: string, public username: string) {
-    constructor(name: string, type: string) {
-        this.name = name;
-        this.type = type;
-        this.healt = 200;
+// Namespaces
+
+namespace MyMath {
+    const PI = 3.14;
+    export function calculateCircumference(diameter: number) {
+        return diameter * PI;
     }
-    printAge() {
-        console.log(this.healt);
-        this.setType("CEO")
-    }
-
-    private setType(type: string) {
-        this.type = type;
-        console.log(this.type)
-    }
-
-}
-const person = new Person('Gaurav', 'Owner');
-person.printAge();
-
-// Inheritance
-
-class ThisUser extends Person {
-    name = "Gaurav";
-    constructor() {
-        super(name, 'Employee');
-        this.healt = 50;
-        // console.log(this.type) can't access!
+    export function calculateRectangle(width: number, length: number) {
+        return width * length;
     }
 }
 
-const gaurav = new ThisUser();
-console.log(gaurav)
+const PI = 2.99;
 
-
-// Getters & Setters
-class Plant {
-    private _species: string = "Default";
-
-    get species() {
-        return this._species.toUpperCase();
-    }
-
-    set species(value: string) {
-        value.length > 3 ? this._species = value : this._species = "Default";
-    }
-}
-
-let plant = new Plant();
-console.log(plant.species);
-
-plant.species = "AB";
-console.log(plant.species);
-
-plant.species = "Green Plant";
-console.log(plant.species);
-
-// Static Properties & Methods
-
-class Helper {
-    static PI: number = 3.14; // by default value
-    static calcCircumference(diameter: number): number {
-        return this.PI * diameter;
-    }
-}
-
-console.log(Helper.PI * 2);
-console.log(Helper.calcCircumference(8));
-
-// Abstract Classes
-
-// it may be helpfull for basic setup for any classes not this class won't instanciat 
-// you can bundle some generall functionallty which should provide basci blue print to child calss
-// but remember it will never instanciat. so, Keep that in mind.
-abstract class Project {
-    projectName: string = "Default";
-    budget: number = 0;
-
-    abstract changeName(name: string): void;
-
-    clacBudget() {
-        return this.budget * 2;
-    }
-}
-
-class ITProject extends Project {
-    changeName(name: string): void {
-        // set the login the way you want to set
-        this.projectName = name;
-    }
-}
-
-
-let newProject = new ITProject();
-console.log(newProject);
-newProject.changeName('Super IT Project');
-console.log(newProject);
-
-// private constructors
-
-class OnlyOne {
-    // Added 'static', bucause we can use it like [OnlyOne.instance]. and
-    // if not 'static' than this will be 'onlyOne = new OnlyOne('the Only One); onlyOne.instance'
-    // which is not what we want
-    private static instance: OnlyOne;
-    public readonly name: string;
-
-    private constructor(name: string) {
-        this.name = name;
-    }
-
-    static getInstance() {
-        if (!OnlyOne.instance) {
-            OnlyOne.instance = new OnlyOne('The Only One');
-        }
-        return OnlyOne.instance;
-    }
-}
-
-// Singleton Class which gives only one instance at runtime.
-
-// let wrong = new OnlyOne('The Only One'); // wrong way
-let right = OnlyOne.getInstance(); // right way
-
-console.log(right.name);
-// right.name = 'Somthing else'; // throw error because this property is readOnly
+console.log(MyMath.calculateRectangle(10, 20));
+console.log(MyMath.calculateCircumference(20));
+console.log(PI);
