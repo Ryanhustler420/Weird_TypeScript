c29c147ba3ff2e1fd77f42fe659a99f4e5a730b7
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-14 20:45:48 +0530

    Function chaning Example

--- app/app.js
+++ app/app.js
@@ -17,3 +17,31 @@ var Person = /** @class */ (function () {
     ], Person);
     return Person;
 }());
+var Auth = /** @class */ (function () {
+    function Auth() {
+        if (Auth.instance) {
+            return Auth.instance;
+        }
+        Auth.instance = this;
+    }
+    Auth.prototype.request = function (url) {
+        Auth.url = url;
+        return this;
+    };
+    Auth.prototype.check = function (username, password, cb) {
+        return cb(true, true);
+    };
+    return Auth;
+}());
+var auth = new Auth();
+auth.request('api/user/auth')
+    .check('gaurav', '123456', function (err, success) {
+    if (err) {
+        return console.log('Error');
+    }
+    console.log('Auth Successfully Completed');
+});
+// let second = new Auth();
+// console.log(first===second);
+// 1. connect to the database according to NODE_ENV
+// 2. Populate data my method call and passes the require data
--- app/app.ts
+++ app/app.ts
@@ -7,3 +7,43 @@ class Person {
         console.log('Hi')
    }
 }
+
+class Auth {
+
+    private static instance: Auth;
+    static url: string;
+
+    constructor() {
+        if (Auth.instance) {
+            return Auth.instance;
+        }
+        Auth.instance = this;
+    }
+
+    request(url: string): Auth {
+        Auth.url = url;
+        return this;
+    }
+
+    check(username: string, password: string, cb: Function): Function {
+        return cb(null, true);
+    }
+}
+
+let auth = new Auth();
+auth.request('api/user/auth')
+    .check('gaurav', '123456', (err: any, success: any) => {
+    if(err){
+        return console.log('Error');
+    }
+    console.log('Auth Successfully Completed')
+})
+
+// let second = new Auth();
+// console.log(first===second);
+
+// database
+// authentication
+
+// 1. connect to the database according to NODE_ENV
+// 2. Populate data my method call and passes the require data
