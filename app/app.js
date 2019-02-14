7ce2f49447bd4f896d0a1f647bac21d26f6c8b5d
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-14 13:08:34 +0530

    Creating a Generic Function

--- app/app.js
+++ app/app.js
@@ -1,57 +1,17 @@
 "use strict";
-// Interfaces
-// function greet(person: any) {
-//     console.log(`hello ${person.name}`);
-// }
-// function greet(person: { name: string }) {
-//     console.log(`hello ${person.name}`);
-// }
-function greet(person) {
-    console.log("hello " + person.firstName);
+// Simple Generic
+function echo(data) {
+    return data;
 }
-// function changeName(person: { name: string }) {
-//     person.name = "Saurav";
-// }
-function changeName(person) {
-    person.firstName = "Saurav";
+console.log(echo('Gaurav'));
+// console.log(echo(27).length); //will throw error because number does'nt have length property
+console.log(echo(27));
+console.log(echo({ name: "Gaurav", age: 27 }));
+// Generic make code very flexible and dynamic
+// Better Generic 
+function betterEcho(data) {
+    return data;
 }
-var person = {
-    firstName: 'Gaurav',
-    hobbies: ['Cooking', 'Sports'],
-    greet: function (lastName) {
-        console.log("Hi I am " + this.firstName + " " + lastName);
-    }
-};
-greet(person);
-// greet({ firstName: 'Gaurav', age: 27 }); // this will check if you pass object literal directly. this will throw error
-// greet({ firstName: 'Gaurav', age: 27 }); // this is possible if you put optional property in NamedPerson interface using ? mark
-// greet({ firstName: 'Gaurav' });
-changeName(person);
-greet(person);
-person.greet('Everyone');
-var Person = /** @class */ (function () {
-    function Person() {
-        this.firstName = '';
-    }
-    Person.prototype.greet = function (lastName) {
-        console.log("Hi I am " + this.firstName + " " + lastName);
-    };
-    return Person;
-}());
-var myPerson = new Person();
-myPerson.firstName = "Ram";
-greet(myPerson);
-myPerson.greet('Everyone');
-var myDoubleFunc;
-myDoubleFunc = function (value1, value2) {
-    return (value1 + value2) * 2;
-};
-console.log(myDoubleFunc(10, 20));
-var oldPerson = {
-    age: 27,
-    firstName: "Gaurav",
-    greet: function (lastName) {
-        console.log('Hello');
-    }
-};
-console.log(oldPerson);
+console.log(betterEcho('Gaurav'));
+console.log(betterEcho(27));
+console.log(betterEcho({ name: "Gaurav", age: 27 }));
--- app/app.ts
+++ app/app.ts
@@ -1,97 +1,19 @@
-// Interfaces
+// Simple Generic
+function echo(data: any) {
+    return data;}
+console.log(echo('Gaurav'));
+// console.log(echo(27).length); //will throw error because number does'nt have length property
+console.log(echo(27));
+console.log(echo({ name: "Gaurav", age: 27 }))
+// Generic make code very flexible and dynamic
 
-
-interface NamedPerson {
-    firstName: string;
-    age?: number; // this could be available or not but the key must be age
-    [propName: string]: any // here we dont know that what the key would be
-    // checkAge: (age: number, currentYear: number) => boolean;
-    greet(lastName: string): void;
-}
-
-
-// function greet(person: any) {
-//     console.log(`hello ${person.name}`);
-// }
-
-// function greet(person: { name: string }) {
-//     console.log(`hello ${person.name}`);
-// }
-
-function greet(person: NamedPerson) {
-    console.log(`hello ${person.firstName}`);
-}
-
-// function changeName(person: { name: string }) {
-//     person.name = "Saurav";
-// }
-
-function changeName(person: NamedPerson) {
-    person.firstName = "Saurav";
-}
-
-const person: NamedPerson = {
-    firstName: 'Gaurav',
-    hobbies: ['Cooking', 'Sports'],
-    greet(lastName: string) {
-        console.log(`Hi I am ${this.firstName} ${lastName}`)
-    }
-}
-
-greet(person);
-// greet({ firstName: 'Gaurav', age: 27 }); // this will check if you pass object literal directly. this will throw error
-// greet({ firstName: 'Gaurav', age: 27 }); // this is possible if you put optional property in NamedPerson interface using ? mark
-// greet({ firstName: 'Gaurav' });
-changeName(person);
-greet(person);
-person.greet('Everyone')
-
-
-class Person implements NamedPerson {
-    firstName: string = '';
-    greet(lastName: string) {
-        console.log(`Hi I am ${this.firstName} ${lastName}`)
-    }
-}
-
-const myPerson = new Person();
-myPerson.firstName = "Ram";
-greet(myPerson);
-myPerson.greet('Everyone');
-
-// Function Types
-
-interface DoubleValueFunc {
-    (number1: number, number2: number): number;
-}
-
-let myDoubleFunc: DoubleValueFunc;
-
-myDoubleFunc = function (value1: number, value2: number) {
-    return (value1 + value2) * 2;
-}
-
-console.log(myDoubleFunc(10, 20));
-
-// interface NamedPerson {
-//     firstName: string;
-//     age?: number;
-//     [propName: string]: any
-//     greet: DoubleValueFunc
-// }
-
-// Interface Inheritance
-
-interface AgedPerson extends NamedPerson {
-    age: number;
-}
-
-const oldPerson: AgedPerson = {
-    age: 27,
-    firstName: "Gaurav",
-    greet(lastName: string) {
-        console.log('Hello');
-    }
-}
-
-console.log(oldPerson);
+// Better Generic 
+function betterEcho<T>(data: T) {
+    return data;}
+    
+// console.log(betterEcho<string>('Gaurav'));
+// console.log(betterEcho<number>(27));
+// console.log(betterEcho<Object>({ name: "Gaurav", age: 27 }))
+console.log(betterEcho('Gaurav').length);
+// console.log(betterEcho(27).length); // throw error
+console.log(betterEcho({ name: "Gaurav", age: 27 }))
