7306085d65d122b68606b46e651def58a6c376cf
Gaurav Gupta <gouravgupta840@gmail.com>
2019-02-14 16:55:52 +0530

    Module Exercise Solution

--- app/app.js
+++ app/app.js
@@ -1,48 +1,30 @@
 "use strict";
-// Simple Generic
-function echo(data) {
-    return data;
-}
-console.log(echo('Gaurav'));
-// console.log(echo(27).length); //will throw error because number does'nt have length property
-console.log(echo(27));
-console.log(echo({ name: "Gaurav", age: 27 }));
-// Generic make code very flexible and dynamic
-// Better Generic 
-function betterEcho(data) {
-    return data;
-}
-// console.log(betterEcho<string>('Gaurav'));
-// console.log(betterEcho<number>(27));
-// console.log(betterEcho<Object>({ name: "Gaurav", age: 27 }))
-console.log(betterEcho('Gaurav').length);
-// console.log(betterEcho(27).length); // throw error
-console.log(betterEcho({ name: "Gaurav", age: 27 }));
-// Build-in Generics
-var testResult = [1.94, 2.33];
-testResult.push(-2.99);
-// testResult.push("String") // wrong
-console.log(testResult);
-// Arrays
-function printALl(args) {
-    args.forEach(console.log);
-}
-printALl(['Apple', 'Banana']);
-printALl(['Apple', 'Banana']);
-var echo2 = betterEcho; // betterEcho function signature fits in this type that why we can assign that function into it
-console.log(echo2('Something'));
-// Generic Class
-var SimpleMath = /** @class */ (function () {
-    function SimpleMath() {
+// Excersice
+var Map = /** @class */ (function () {
+    function Map() {
+        this.map = {};
     }
-    SimpleMath.prototype.calculate = function () {
-        return +this.baseValue * +this.multiplyValue;
+    Map.prototype.setItem = function (key, item) {
+        this.map[key] = item;
     };
-    return SimpleMath;
+    Map.prototype.getItem = function (key) {
+        return this.map[key];
+    };
+    Map.prototype.clear = function () {
+        this.map = {};
+    };
+    Map.prototype.printMap = function () {
+        for (var key in this.map) {
+            console.log(key, this.map[key]);
+        }
+    };
+    return Map;
 }());
-// const simpleMath = new SimpleMath();
-var simpleMath = new SimpleMath();
-simpleMath.baseValue = 10;
-// simpleMath.baseValue = '10'; throw error
-simpleMath.multiplyValue = 30;
-console.log(simpleMath.calculate());
+var numberMap = new Map();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+numberMap.printMap();
+var stringMap = new Map();
+stringMap.setItem('name', 'Gaurav');
+stringMap.setItem('age', "27");
+stringMap.printMap();
--- app/app.ts
+++ app/app.ts
@@ -1,63 +1,28 @@
-// Simple Generic
-function echo(data: any) {
-    return data;}
-console.log(echo('Gaurav'));
-// console.log(echo(27).length); //will throw error because number does'nt have length property
-console.log(echo(27));
-console.log(echo({ name: "Gaurav", age: 27 }))
-// Generic make code very flexible and dynamic
-
-// Better Generic 
-function betterEcho<T>(data: T) {
-    return data;}
-
-// console.log(betterEcho<string>('Gaurav'));
-// console.log(betterEcho<number>(27));
-// console.log(betterEcho<Object>({ name: "Gaurav", age: 27 }))
-console.log(betterEcho('Gaurav').length);
-// console.log(betterEcho(27).length); // throw error
-console.log(betterEcho({ name: "Gaurav", age: 27 }))
-
-
-// Build-in Generics
-const testResult: Array<number> = [1.94,2.33];
-testResult.push(-2.99);
-// testResult.push("String") // wrong
-console.log(testResult);
-
-// Arrays
-
-function printALl<T>(args: T[]) {
-    args.forEach(console.log);
-}
-
-printALl(['Apple','Banana']);
-printALl<string>(['Apple','Banana']);
-
-// Generic Types
-
-// const echo2: <T>(data: T) => T = echo;
-
-type funcType = <T>(data: T) => T;
-const echo2: funcType = betterEcho; // betterEcho function signature fits in this type that why we can assign that function into it
-
-console.log(echo2<string>('Something'))
-
-// Generic Class
-type N_OR_S = number | string;
-
-// class SimpleMath <T extends N_OR_S, U extends N_OR_S> {
-class SimpleMath <T extends U, U extends N_OR_S> {
-    baseValue: T;
-    multiplyValue: U;
-    calculate(): number {
-        return +this.baseValue * +this.multiplyValue;
+// Excersice
+type KEY_ONLY_STRING = string;
+class Map<T extends number | string> {
+    private map: { [key: string]: T } = {}
+    setItem(key: KEY_ONLY_STRING, item: T) {
+        this.map[key] = item;
+    }
+    getItem(key: KEY_ONLY_STRING) {
+        return this.map[key];
+    }
+    clear() {
+        this.map = {};
+    }
+    printMap() {
+        for (let key in this.map) {
+            console.log(key, this.map[key]);
+        }
     }
 }
+const numberMap = new Map<number>();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+numberMap.printMap();
+const stringMap = new Map<string>();
+stringMap.setItem('name', 'Gaurav')
+stringMap.setItem('age', "27");
+stringMap.printMap();
 
-// const simpleMath = new SimpleMath();
-const simpleMath = new SimpleMath<number, number>();
-simpleMath.baseValue = 10;
-// simpleMath.baseValue = '10'; throw error
-simpleMath.multiplyValue = 30;
-console.log(simpleMath.calculate());
